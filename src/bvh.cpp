9bf908aa04ac39c3cbe2bf7ba05831669d139292
Shanaka <shanaka95@gmail.com>
2024-12-24 12:56:21 +0100

    cuda fixes

--- src/bvh.cpp
+++ src/bvh.cpp
@@ -22,7 +22,7 @@
 void bvh_cuda_forward(at::Tensor triangles, at::Tensor* collision_tensor_ptr,
         int max_collisions = 16);
 
-#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
